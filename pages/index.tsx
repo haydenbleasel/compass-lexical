0862c6aea91105fbf461e4bf712dc919b55c7173
Hayden Bleasel <hello@haydenbleasel.com>
2022-10-29 18:14:49 -0400

    Add basic Lexical setup

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,5 +1,77 @@
+import type { EditorState } from 'lexical';
+import { $getRoot, $getSelection } from 'lexical';
 import type { FC } from 'react';
+import { useEffect } from 'react';
 
-const Home: FC = () => <p>Hello, world.</p>;
+import { LexicalComposer } from '@lexical/react/LexicalComposer';
+import { PlainTextPlugin } from '@lexical/react/LexicalPlainTextPlugin';
+import { ContentEditable } from '@lexical/react/LexicalContentEditable';
+import { HistoryPlugin } from '@lexical/react/LexicalHistoryPlugin';
+import { OnChangePlugin } from '@lexical/react/LexicalOnChangePlugin';
+import { useLexicalComposerContext } from '@lexical/react/LexicalComposerContext';
 
-export default Home;
+const theme = {
+  // Theme styling goes here
+};
+
+/*
+ * When the editor changes, you can get notified via the
+ * LexicalOnChangePlugin!
+ */
+const onChange = (editorState: EditorState) => {
+  editorState.read(() => {
+    // Read the contents of the EditorState here.
+    const root = $getRoot();
+    const selection = $getSelection();
+
+    console.log(root, selection);
+  });
+};
+
+/*
+ * Lexical React plugins are React components, which makes them
+ * highly composable. Furthermore, you can lazy load plugins if
+ * desired, so you don't pay the cost for plugins until you
+ * actually use them.
+ */
+const MyCustomAutoFocusPlugin = () => {
+  const [editor] = useLexicalComposerContext();
+
+  useEffect(() => {
+    // Focus the editor when the effect fires!
+    editor.focus();
+  }, [editor]);
+
+  return null;
+};
+
+/*
+ * Catch any errors that occur during Lexical updates and log them
+ * or throw them as needed. If you don't throw them, Lexical will
+ * try to recover gracefully without losing user data.
+ */
+const onError = (error) => {
+  console.error(error);
+};
+
+const Editor: FC = () => {
+  const initialConfig = {
+    namespace: 'MyEditor',
+    theme,
+    onError,
+  };
+
+  return (
+    <LexicalComposer initialConfig={initialConfig}>
+      <PlainTextPlugin
+        contentEditable={<ContentEditable />}
+        placeholder={<div>Enter some text...</div>}
+      />
+      <OnChangePlugin onChange={onChange} />
+      <HistoryPlugin />
+      <MyCustomAutoFocusPlugin />
+    </LexicalComposer>
+  );
+};
+
+export default Editor;
