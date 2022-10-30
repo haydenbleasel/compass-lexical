36b25a9f617386918cc8fcdbcc481ef5aa3fac66
Hayden Bleasel <hello@haydenbleasel.com>
2022-10-30 12:45:17 -0400

    Attempt to fix Firebase initialization

--- lib/firebase.ts
+++ lib/firebase.ts
@@ -1,3 +1,4 @@
+import type { FirebaseApp } from 'firebase/app';
 import { initializeApp } from 'firebase/app';
 import { connectAuthEmulator, getAuth } from 'firebase/auth';
 import { connectFirestoreEmulator, getFirestore } from 'firebase/firestore';
@@ -18,11 +19,26 @@ const firebaseConfig = {
   measurementId: process.env.NEXT_PUBLIC_FIREBASE_MEASUREMENTID,
 };
 
-// eslint-disable-next-line jest/require-hook
-const app = initializeApp(firebaseConfig);
-// const analytics = getAnalytics(app);
+export const firebase = (): FirebaseApp => {
+  // eslint-disable-next-line jest/require-hook
+  const app = initializeApp(firebaseConfig);
+  // const analytics = getAnalytics(app);
 
-if (process.env.NODE_ENV === 'production') {
+  if (process.env.NODE_ENV !== 'production') {
+    const auth = getAuth();
+    connectAuthEmulator(auth, 'http://localhost:9099');
+
+    const firestore = getFirestore();
+    connectFirestoreEmulator(firestore, 'localhost', 8080);
+
+    const storage = getStorage();
+    connectStorageEmulator(storage, 'localhost', 9199);
+  }
+
+  return app;
+};
+
+export const appCheck = (app: FirebaseApp): void => {
   initializeAppCheck(app, {
     provider: new ReCaptchaV3Provider(
       process.env.NEXT_PUBLIC_RECAPTCHA_SITEKEY ?? ''
@@ -30,13 +46,4 @@ if (process.env.NODE_ENV === 'production') {
 
     isTokenAutoRefreshEnabled: true,
   });
-} else {
-  const auth = getAuth();
-  connectAuthEmulator(auth, 'http://localhost:9099');
-
-  const firestore = getFirestore();
-  connectFirestoreEmulator(firestore, 'localhost', 8080);
-
-  const storage = getStorage();
-  connectStorageEmulator(storage, 'localhost', 9199);
-}
+};
--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,68 +1,79 @@
 import '../styles/globals.css';
 import type { AppProps } from 'next/app';
 import type { FC } from 'react';
+import { useEffect } from 'react';
 import { Toaster } from 'react-hot-toast';
 import { Provider as TooltipProvider } from '@radix-ui/react-tooltip';
 import { Analytics } from '@vercel/analytics/react';
-import '../lib/firebase';
 import { DefaultSeo } from 'next-seo';
 import Head from 'next/head';
+import { firebase, appCheck } from '../lib/firebase';
 
 const title = 'Compass — a simple, ephemeral notes app';
 const description =
   'Compass is a simple app that lets you write Markdown-enabled notes.';
 
-const App: FC<AppProps> = ({ Component, pageProps }) => (
-  <>
-    <DefaultSeo
-      title={title}
-      description={description}
-      openGraph={{
-        type: 'website',
-        locale: 'en_US',
-        url: 'https://compass.haydenbleasel.com/',
-        site_name: 'Compass',
-        images: [
-          {
-            url: `https://compass.haydenbleasel.com/api/og?title=${title}&description=${description}`,
-            width: 1200,
-            height: 630,
-            alt: 'Compass',
-          },
-        ],
-      }}
-      twitter={{
-        handle: '@haydenbleasel',
-        site: '@haydenbleasel',
-        cardType: 'summary_large_image',
-      }}
-    />
-    <Head>
-      <link
-        rel="apple-touch-icon"
-        sizes="180x180"
-        href="/apple-touch-icon.png"
+const app = firebase();
+
+const App: FC<AppProps> = ({ Component, pageProps }) => {
+  useEffect(() => {
+    if (typeof window !== 'undefined') {
+      appCheck(app);
+    }
+  }, []);
+
+  return (
+    <>
+      <DefaultSeo
+        title={title}
+        description={description}
+        openGraph={{
+          type: 'website',
+          locale: 'en_US',
+          url: 'https://compass.haydenbleasel.com/',
+          site_name: 'Compass',
+          images: [
+            {
+              url: `https://compass.haydenbleasel.com/api/og?title=${title}&description=${description}`,
+              width: 1200,
+              height: 630,
+              alt: 'Compass',
+            },
+          ],
+        }}
+        twitter={{
+          handle: '@haydenbleasel',
+          site: '@haydenbleasel',
+          cardType: 'summary_large_image',
+        }}
       />
-      <link
-        rel="icon"
-        type="image/png"
-        sizes="32x32"
-        href="/favicon-32x32.png"
-      />
-      <link
-        rel="icon"
-        type="image/png"
-        sizes="16x16"
-        href="/favicon-16x16.png"
-      />
-      <link rel="manifest" href="/site.webmanifest" />
-    </Head>
-    <TooltipProvider delayDuration={0}>
-      <Component {...pageProps} />
-      <Toaster position="bottom-right" />
-    </TooltipProvider>
-    <Analytics />
-  </>
-);
+      <Head>
+        <link
+          rel="apple-touch-icon"
+          sizes="180x180"
+          href="/apple-touch-icon.png"
+        />
+        <link
+          rel="icon"
+          type="image/png"
+          sizes="32x32"
+          href="/favicon-32x32.png"
+        />
+        <link
+          rel="icon"
+          type="image/png"
+          sizes="16x16"
+          href="/favicon-16x16.png"
+        />
+        <link rel="manifest" href="/site.webmanifest" />
+      </Head>
+      <TooltipProvider delayDuration={0}>
+        <Component {...pageProps} />
+        <Toaster position="bottom-right" />
+      </TooltipProvider>
+      <Analytics />
+    </>
+  );
+};
 
 export default App;
