2d183b81cc39879ce936f45daa757c9ab056be5a
Hayden Bleasel <hello@haydenbleasel.com>
2022-10-30 02:31:18 -0400

    Add basic metadata

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -5,9 +5,17 @@ import { Toaster } from 'react-hot-toast';
 import { Provider as TooltipProvider } from '@radix-ui/react-tooltip';
 import { Analytics } from '@vercel/analytics/react';
 import '../lib/firebase';
+import Head from 'next/head';
 
 const App: FC<AppProps> = ({ Component, pageProps }) => (
   <>
+    <Head>
+      <title>Compass — a simple, ephemeral notes app</title>
+      <meta
+        name="description"
+        content="Compass is a simple app that lets you write Markdown-enabled notes."
+      />
+    </Head>
     <TooltipProvider delayDuration={0}>
       <Component {...pageProps} />
       <Toaster position="bottom-right" />
